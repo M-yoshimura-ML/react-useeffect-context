e3be0860476dab43c6cff47043c239bca83ad37c
M-yoshimura-ML <masahiro.yoshimura@freemight.com>
2022-07-13 16:21:21 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { useContext } from 'react';
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
+// import { useEffect } from 'react';
+import AuthContext from './store/auth-context';
+
+function App() {
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // useEffect(() => {
+  //   const storedUserLoginInfo = localStorage.getItem('isLoggedIn');
+
+  //   if(storedUserLoginInfo === '1') {
+  //     setIsLoggedIn(true);
+  //   }
+  // }, []);
+
+  // const loginHandler = (email, password) => {
+  //   localStorage.setItem('isLoggedIn', '1');
+  //   setIsLoggedIn(true);
+  // };
+
+  // const logoutHandler = () => {
+  //   localStorage.removeItem('isLoggedIn');
+  //   setIsLoggedIn(false);
+  // };
+  const ctx = useContext(AuthContext);
+
+  return (
+    // <AuthContext.Provider 
+    //   value={{
+    //     isLoggedIn:isLoggedIn,
+    //     onLogout: logoutHandler
+    //   }}
+    // >
+    //   {/* <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} /> */}
+    //   <MainHeader onLogout={logoutHandler} />
+    //   <main>
+    //     {!isLoggedIn && <Login onLogin={loginHandler} />}
+    //     {isLoggedIn && <Home onLogout={logoutHandler} />}
+    //   </main>
+    // </AuthContext.Provider>
+    <React.Fragment>
+      <MainHeader />
+      <main>
+        {!ctx.isLoggedIn && <Login />}
+        {ctx.isLoggedIn && <Home />}
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
